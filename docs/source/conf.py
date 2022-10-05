12bcedc1674285354579266a56a04512d99d1b96
Luighi <luighiavz@gmail.com>
2022-10-04 22:36:12 -0500

    Add initial contents to course

---
+++ docs/source/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.append(os.path.abspath("./_ext"))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Software development tools'
+copyright = '2022, Luighi Viton'
+author = 'Luighi Viton'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autosectionlabel',
+    'google_analytics',
+    "sphinxext.opengraph",
+]
+
+autosectionlabel_prefix_document = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
+html_static_path = ['_static']
+
+html_extra_path = ['CNAME', 'robots.txt']
+
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# Add GA tag
+google_analytics_id = os.environ.get('CI_GOOGLE_ANALYTICS_ID', None)
+
+# Add OG metatags
+ogp_site_url = "http://swdev.courses.luighiviton.com/"
+ogp_image = "http://swdev.courses.luighiviton.com/_static/banner-es.png"
+ogp_description_length = 300
+ogp_type = "article"
---
+++ docs/source/index.rst
@@ -0,0 +1,47 @@
+.. Software development tools documentation master file, created by
+   sphinx-quickstart on Tue Oct  4 10:31:50 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Curso Tecnologías de Desarrollo de Software
+======================================================
+
+:Autor: Luighi Vitón Zorrilla (https://luighiviton.com)
+:Fecha de creación: 2022-10-05
+:Fecha de actualización: 2022-10-05
+
+Información del curso
+---------------------
+
+Descripción
+~~~~~~~~~~~
+
+Este curso es una iniciativa para efectuar una introducción a herramientas,
+conceptos y tenologías empleados en el desarrollo de software. 
+
+Estructura del curso
+~~~~~~~~~~~~~~~~~~~~
+
+El curso se divide en los siguientes temas.
+
+1. Scripting en Linux: Bash
+
+2. Control de versiones: Uso de Git / GitHub
+
+3. Bases de datos: PostgreSQL
+
+4. Desarrollo en backend: ExpressJS
+
+5. Desarrollo en frontend: ReactJS
+
+6. Gestión de aplicaciones: Docker / Docker-compose
+
+7. Despliegue en la nube: Google Cloud
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Tabla de contenidos:
+
+   contents/index
+
